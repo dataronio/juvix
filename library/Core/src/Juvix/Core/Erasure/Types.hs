f70bcfc8abfa710f53d5ba2f7c9ad61a5d8ee830
April Gonçalves <april@cyberglot.me>
2021-06-23 12:15:17 +0100

    Add full comments to Core.Erasure.Types

--- library/Core/src/Juvix/Core/Erasure/Types.hs
+++ library/Core/src/Juvix/Core/Erasure/Types.hs
@@ -32,7 +32,9 @@ import qualified Juvix.Library.PrettyPrint as PP
 import Juvix.Library.Usage (Usage)
 
  -- | `MapPrim` seems to convert `p1` into `p2` given a list of names,
- -- | not sure what it's supposed to abstract
+ -- | `ty` and `val` exist for error purposes only
+ -- TODO `MapPrim ty val` probably makes more sense,
+ -- since it can be curried for `ty` and `val` first
  -- TODO MapPrim should probably be re-written as `newtype`
 type MapPrim p1 p2 ty val =
   [NameSymbol.T] -> p1 -> Either (Error ty val) p2
@@ -51,11 +53,13 @@ data Env primTy1 primTy2 primVal1 primVal2 = Env
   }
   deriving (Generic)
 
+-- | MonadTrans composing Exception, Error, State and Env
 type EnvEraAlias primTy1 primTy2 primVal1 primVal2 =
   ExceptT
     (Error primTy1 primVal1)
     (State (Env primTy1 primTy2 primVal1 primVal2))
 
+ -- | Promotes EnvEraAlias to a capability
 newtype EnvT primTy1 primTy2 primVal1 primVal2 a
   = EnvEra (EnvEraAlias primTy1 primTy2 primVal1 primVal2 a)
   deriving (Functor, Applicative, Monad)
@@ -92,6 +96,7 @@ exec ::
   Either (Error primTy1 primVal1) a
 exec mt mv (EnvEra m) = evalState (runExceptT m) (Env 0 [] mt mv)
 
+ -- | Erasure error definition
 data Error primTy primVal
   = UnsupportedTermT (Typed.Term' primTy primVal)
   | UnsupportedTermE (Typed.Elim' primTy primVal)
@@ -101,6 +106,7 @@ data Error primTy primVal
   | InternalError Text
   deriving (Generic)
 
+ -- Boilerplate for `Error` to work with the rest of the codebase
 deriving instance
   ( Show primTy,
     Show primVal,
@@ -154,8 +160,12 @@ instance
     InternalError txt ->
       PP.text txt
 
+ -- | `Core.Erasure.T`
 data T primTy
 
+-- | Is this a top-level `do` ?
+-- | Shouldn't we handle this more gracefully? ^^"
+-- | Also, why do we need to do this?
 do
   primTy' <- Ext.newName "primTy"
   let primTy = Ext.varT primTy'
@@ -173,11 +183,13 @@ do
           Erased.typeApp = typed
         }
 
+ -- TODO Switch to `newtype`
 type TermT primTy primVal = Term primTy (Typed.TypedPrim primTy primVal)
 
 -- TODO: Figure out how to do this with extensible.
 -- IR.extendDatatype "Datatype" [] [t|T|] extDatatype
 
+  -- | Is it supposed to model type declarations?
 data Datatype primTy primVal = Datatype
   { dataName :: GlobalName,
     dataArgs :: [DataArg primTy],
@@ -188,6 +200,7 @@ data Datatype primTy primVal = Datatype
 -- TODO: Figure out how to do this with extensible.
 -- IR.extendDataArg "DataArg" [] [t|T|] extDataArg
 
+ -- What's `DataArg` supposed to model? Type Arguments?
 data DataArg primTy = DataArg
   { argName :: GlobalName,
     argUsage :: Usage,
@@ -197,6 +210,7 @@ data DataArg primTy = DataArg
 -- TODO: Figure out how to do this with extensible.
 -- IR.extendDataCon "DataCon" [] [t|T|] extDataCon
 
+ -- What's `DataCon` supposed to model? Constants?
 data DataCon primTy primVal = DataCon
   { conName :: GlobalName,
     conType :: Type primTy,
@@ -206,6 +220,7 @@ data DataCon primTy primVal = DataCon
 -- TODO: Figure out how to do this with extensible.
 -- IR.extendFunction "Function" [] [t|T|] extFunction
 
+-- | Lambda abstraction
 data Function primTy primVal = Function
   { funName :: GlobalName,
     funUsage :: GlobalUsage,
@@ -213,50 +228,63 @@ data Function primTy primVal = Function
     funClauses :: NonEmpty (FunClause primTy primVal)
   }
 
+ -- | Lambda abstraction extended to primitive types
 type FunctionT primTy primVal =
   Function primTy (Typed.TypedPrim primTy primVal)
 
 -- TODO: Figure out how to do this with extensible.
 -- IR.extendFunClause "FunClause" [] [t|T|] extFunClause
 
+ -- | Function-level pattern matching
 data FunClause primTy primVal
   = FunClause [Pattern primTy primVal] (Term primTy primVal)
 
+ -- | Function-level pattern matching
 type FunClauseT primTy primVal =
   FunClause primTy (Typed.TypedPrim primTy primVal)
 
 -- TODO: Figure out how to do this with extensible.
 -- IR.extendPattern "Pattern" [] [t|T|] extPattern
 
+ -- | `Pattern` models the current patterns available for match?
 data Pattern primTy primVal
+  -- `PCon` is for constants?
   = PCon GlobalName [Pattern primTy primVal]
   | PPair (Pattern primTy primVal) (Pattern primTy primVal)
   | PUnit
   | PVar PatternVar
+  -- `PDot` is for record field lookup?
   | PDot (Term primTy primVal)
   | PPrim primVal
 
+ -- `PatternT` extends `Patterns` to primitive types
 type PatternT primTy primVal =
   Pattern primTy (Typed.TypedPrim primTy primVal)
 
+ -- What's `Abstract` supposed to model?
 data Abstract primTy = Abstract
   { absName :: GlobalName,
     absUsage :: GlobalUsage,
     absType :: Type primTy
   }
 
+ -- | `Global` models terms available in the global scope
 data Global primTy primVal
   = GDatatype (Datatype primTy primVal)
   | GDataCon (DataCon primTy primVal)
   | GFunction (Function primTy primVal)
   | GAbstract (Abstract primTy)
 
+ -- | `Globals` models the global scope
 type Globals primTy primVal = HM.HashMap GlobalName (Global primTy primVal)
 
+ -- | `GlobalT` models primitive types
 type GlobalT primTy primVal = Global primTy (Typed.TypedPrim primTy primVal)
 
+ -- | `GlobalsT` models a global scope of primitive types
 type GlobalsT primTy primVal = Globals primTy (Typed.TypedPrim primTy primVal)
 
+ -- | `getType` returns the type within a term
 getType :: Term primTy primVal -> Type primTy
 getType (Var _ ty) = ty
 getType (Prim _ ty) = ty
@@ -266,6 +294,7 @@ getType (Unit ty) = ty
 getType (Let _ _ _ (_, ty)) = ty
 getType (App _ _ ty) = ty
 
+ -- | `eraseAnn` converts `IR.Term` into `Erased.Term`
 eraseAnn :: Term primTy primVal -> Erased.Term primVal
 eraseAnn (Var sym _) = Erased.Var sym
 eraseAnn (Prim p _) = Erased.Prim p
