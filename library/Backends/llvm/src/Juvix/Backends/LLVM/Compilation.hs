728148fe41127a0f60c1a24e3f1a2d440bcccb0e
Frank Dedden <frank@heliax.dev>
2021-08-20 10:28:11 +0200

    Added a very basic environment for locally bound variables.

--- library/Backends/llvm/src/Juvix/Backends/LLVM/Compilation.hs
+++ library/Backends/llvm/src/Juvix/Backends/LLVM/Compilation.hs
@@ -9,7 +9,8 @@ import Juvix.Backends.LLVM.Primitive
 import qualified Juvix.Core.Erased.Ann as ErasedAnn
 import Juvix.Library
 import Juvix.Library.Feedback
-import qualified LLVM.AST as LLVM (Module, Operand)
+import qualified Juvix.Library.NameSymbol as NameSymbol
+import qualified LLVM.AST as LLVM (Module, Name, Operand)
 import qualified LLVM.AST.Type as LLVM
 import qualified LLVM.IRBuilder as LLVM
 import qualified LLVM.Pretty as LLVM
@@ -23,42 +24,53 @@ compileProgram t@(ErasedAnn.Ann usage ty _) = do
   let llvmmod :: LLVM.Module
       llvmmod = LLVM.buildModule "juvix-module" $ do
         LLVM.function "main" [] (typeToLLVM ty) $ \[] -> do
-          out <- compileTerm t
+          out <- compileTerm [] t
           LLVM.ret out
   return $ toStrict $ LLVM.ppllvm llvmmod
 
+type Env = [(NameSymbol.T, LLVM.Name)]
+
 compileTerm ::
+  Env ->
   ErasedAnn.AnnTerm PrimTy RawPrimVal ->
   LLVM.IRBuilderT LLVM.ModuleBuilder LLVM.Operand
-compileTerm (ErasedAnn.Ann usage ty t) = case t of
-  ErasedAnn.Prim t' -> mkPrim t
-  ErasedAnn.AppM f xs -> mkApp (ErasedAnn.term f) ty xs
+compileTerm env (ErasedAnn.Ann usage ty t) = case t of
+  ErasedAnn.Var symbol -> case P.lookup symbol env of
+    Nothing -> P.error "Variable not found." -- TODO improve error message.
+    Just var -> undefined -- TODO: return variable var
+  ErasedAnn.Prim t' -> mkPrim env t
+  ErasedAnn.AppM f xs -> mkApp env (ErasedAnn.term f) ty xs
 
 mkPrim ::
+  Env ->
   ErasedAnn.Term PrimTy RawPrimVal ->
   LLVM.IRBuilderT LLVM.ModuleBuilder LLVM.Operand
-mkPrim (ErasedAnn.Prim prim) = case prim of
+mkPrim _ (ErasedAnn.Prim prim) = case prim of
   LitInt i -> return $ LLVM.int8 i -- TODO deal with the type correctly.
 
 mkApp ::
+  Env ->
+  -- | The function term of an application.
   ErasedAnn.Term PrimTy RawPrimVal ->
   ErasedAnn.Type PrimTy ->
+  -- | The arguments to the application.
   [ErasedAnn.AnnTerm PrimTy RawPrimVal] ->
   LLVM.IRBuilderT LLVM.ModuleBuilder LLVM.Operand
-mkApp f (ErasedAnn.PrimTy ty) xs = case f of
-  ErasedAnn.Prim prim -> applyPrim prim ty xs
+mkApp env f (ErasedAnn.PrimTy ty) xs = case f of
+  ErasedAnn.Prim prim -> applyPrim env prim ty xs
 
 applyPrim ::
+  Env ->
   RawPrimVal ->
   PrimTy ->
   [ErasedAnn.AnnTerm PrimTy RawPrimVal] ->
   LLVM.IRBuilderT LLVM.ModuleBuilder LLVM.Operand
-applyPrim f ty xs
+applyPrim env f ty xs
   | arityRaw f == lengthN xs =
     case f of
       Add -> do
-        x <- compileTerm (xs P.!! 0)
-        y <- compileTerm (xs P.!! 1)
+        x <- compileTerm env (xs P.!! 0)
+        y <- compileTerm env (xs P.!! 1)
         LLVM.add x y
 
 typeToLLVM :: ErasedAnn.Type PrimTy -> LLVM.Type
