27103a7446e26ef4b8418bf57f1a7c38e414c94e
mariari <mariari@protonmail.com>
2021-08-25 10:16:28 -0500

    Start a running environment for testing, and chain function

---
+++ library/StandardLibrary/src/Juvix/Library/Trace/Environment.hs
@@ -0,0 +1,44 @@
+{-# LANGUAGE DerivingVia #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+
+-- | This module gives a minimal environment to run Traces, best used
+-- for testing
+module Juvix.Library.Trace.Environment where
+
+
+import Juvix.Library
+import Juvix.Library.Trace.Types
+
+
+newtype Minimal = Minimal
+  { trace :: T
+  }
+  deriving (Generic, Show)
+
+type MinimalAlias = State Minimal
+
+type MinimalAliasIO = StateT Minimal IO
+
+newtype MinimalM a = Ctx {_run :: MinimalAlias a}
+  deriving (Functor, Applicative, Monad)
+  deriving
+    ( HasState "trace" T,
+      HasSource "trace" T,
+      HasSink "trace" T
+    )
+    via StateField "trace" MinimalAlias
+
+newtype MinimalMIO a = CtxIO (MinimalAliasIO a)
+  deriving (Functor, Applicative, Monad)
+  deriving
+    ( HasState "trace" T,
+      HasSource "trace" T,
+      HasSink "trace" T
+    )
+    via StateField "trace" MinimalAliasIO
+
+run :: MinimalM a -> Minimal -> (a, Minimal)
+run (Ctx c) = runState c
+
+runIO :: MinimalMIO a -> Minimal -> IO (a, Minimal)
+runIO (CtxIO c) = runStateT c
--- library/StandardLibrary/src/Juvix/Library/Trace/Format.hs
+++ library/StandardLibrary/src/Juvix/Library/Trace/Format.hs
@@ -1,16 +1,59 @@
 module Juvix.Library.Trace.Format where
 
-
-import Juvix.Library.Trace.Types
 import Control.Lens (over, set, (^.))
-import qualified Data.Text as T
+import qualified Data.Text as Text
 import Juvix.Library
 import qualified Juvix.Library.HashMap as HashMap
 import qualified Juvix.Library.NameSymbol as NameSymbol
-
-
-
+import Juvix.Library.Trace.Types
 
 --------------------------------------------------------------------------------
--- Helpers
+-- Main Functionality
 --------------------------------------------------------------------------------
+
+currentStackChain :: StackChain -> (Int -> Int) -> ([Char], Int)
+currentStackChain chain indentationIncrement =
+  chain
+    |> stackChainToList
+    -- Let's start with the parent
+    |> foldr f ("", 0)
+  where
+    f current (formattedString, indentation) =
+      ( formattedString
+          <> spacing indentation
+          <> functionCall (current ^. name) (current ^. start)
+          <> "\n",
+        indentationIncrement indentation
+      )
+
+--------------------------------------------------------------------------------
+-- Formatting Helpers
+--------------------------------------------------------------------------------
+
+customSpacing :: Char -> Int -> [Char]
+customSpacing = flip replicate
+
+spacing :: Int -> [Char]
+spacing = customSpacing '·'
+
+functionCall :: NameSymbol.T -> [Text] -> [Char]
+functionCall f args =
+  "("
+    <> unintern (NameSymbol.toSymbol f)
+    <> space
+    <> foldMap Text.unpack (intersperse " " args)
+    <> ")"
+    where
+      space =
+        case args of
+          [] -> ""
+          _: _ -> " "
+
+--------------------------------------------------------------------------------
+-- Utility Helpers
+--------------------------------------------------------------------------------
+
+stackChainToList :: StackChain -> [Stack]
+stackChainToList Empty = []
+stackChainToList StackChain {parent = cdr, currentStack = car} =
+  car : stackChainToList cdr
