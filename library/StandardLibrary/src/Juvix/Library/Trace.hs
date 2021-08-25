a6b831e4cf8b93e01f918040bf8cac07135a5a8a
mariari <mariari@protonmail.com>
2021-08-25 09:20:48 -0500

    Move Types to their own File to be able to make more modules

--- library/StandardLibrary/src/Juvix/Library/Trace.hs
+++ library/StandardLibrary/src/Juvix/Library/Trace.hs
@@ -5,78 +5,15 @@
 --
 -- - The structure requires your code to exist in the =Trace.Eff=
 --   effect.
-module Juvix.Library.Trace where
+module Juvix.Library.Trace (module Juvix.Library.Trace, module Juvix.Library.Trace.Types) where
 
+import Juvix.Library.Trace.Types
 import Control.Lens (over, set, (^.))
-import qualified Control.Lens as Lens hiding ((|>))
 import qualified Data.Text as T
+import qualified Juvix.Library.Trace.Format as Format
 import Juvix.Library
 import qualified Juvix.Library.HashMap as HashMap
 import qualified Juvix.Library.NameSymbol as NameSymbol
-
-data T = T
-  { -- | @tCurrent@ represents the current stack-trace we are computing
-    -- under, We use a StackChain to determine the trace the current
-    -- trace is computing under.
-    tCurrent :: StackChain,
-    -- | @tTraces@ represents our current trace log of all completed
-    -- functions
-    tTraces :: Log,
-    -- | @tEnabled@ represents meta information our system has regards
-    -- to if particular trace functions are enabled and if there is a
-    -- general debug level we can print out instead.
-    tEnabled :: HashMap.T NameSymbol.T MetaInfo,
-    -- | @tDebugLevel@ represents the debug level we care about
-    tDebugLevel :: Maybe Natural
-  }
-  deriving (Show)
-
-type Log = [Stack]
-
-data StackChain
-  = Empty
-  | StackChain
-      { parent :: StackChain,
-        currentStack :: Stack
-      }
-  deriving (Show)
-
--- | @Stack@ represents a stack-trace for a particular function
-data Stack = Stack
-  { -- | @stackName@ represents the name of the stackd function
-    stackName :: NameSymbol.T,
-    -- | @stackdStart@ represents the incoming function arguments
-    stackStart :: [T.Text],
-    -- | @stackBetween@ represents the Traces that have happened
-    -- between this call and the end of the call
-    stackBetween :: Log,
-    -- | @stackOutput@ represents the output result. Maybe as we could
-    -- stop before it's finished!
-    stackOutput :: Maybe T.Text
-  }
-  deriving (Show)
-
-data MetaInfo = MetaInfo
-  { metaInfoEnable :: Enable,
-    metaInfoLevel :: Natural
-  }
-  deriving (Show)
-
-data Enable
-  = -- | @Enabled@ represents a trace enabled function
-    Enabled
-  | -- | @Disabled@ represents that the function is disabled
-    Disabled
-  | -- | @DisableRecursive@ represents that we shouldn't trace any
-    -- Enabled functions inside the scope of the Trace
-    DisableRecursive
-  deriving (Show)
-
-Lens.makeLensesWith Lens.camelCaseFields ''Stack
-Lens.makeLensesWith Lens.camelCaseFields ''T
-Lens.makeLensesWith Lens.camelCaseFields ''MetaInfo
-
-type Eff m = HasState "trace" T m
 
 --------------------------------------------------------------------------------
 -- Core API
@@ -95,6 +32,12 @@ withScope name args f = do
   ret <- f
   modify @"trace" (finishScope . (`registerOutput` show ret))
   pure ret
+
+format = undefined
+
+-- | @break@ dumps the current Stack Trace, Ignoring any disabling behavior
+break t = undefined
+
 
 --------------------------------------------------------------------------------
 -- Helper Functionality
