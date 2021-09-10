f5826d90b10b18d4cb01a388b1e891d026470d64
Terence Rokop <rokopt@sharpears.net>
2021-09-09 17:22:47 -0700

    Correct erasure of (co)product types/terms

--- library/Core/src/Juvix/Core/Erased/Algorithm.hs
+++ library/Core/src/Juvix/Core/Erased/Algorithm.hs
@@ -169,23 +169,17 @@ eraseTerm (Typed.Pair s t ann) = do
 eraseTerm t@Typed.CatProduct {} = throwEra $ Erasure.UnsupportedTermT t
 eraseTerm t@Typed.CatCoproduct {} = throwEra $ Erasure.UnsupportedTermT t
 eraseTerm (Typed.CatProductIntro s t ann) = do
-  let ty@(IR.VCatProduct _ _) = Typed.annType ann
-  Erasure.CatProductIntro <$> eraseTerm s <*> eraseTerm t <*> eraseType ty
+  Erasure.CatProductIntro <$> eraseTerm s <*> eraseTerm t <*> eraseType (Typed.annType ann)
 eraseTerm (Typed.CatProductElimLeft a s ann) = do
-  let ty@(IR.VCatProduct _ _) = Typed.annType ann
-  Erasure.CatProductElimLeft <$> eraseTerm a <*> eraseTerm s <*> eraseType ty
+  Erasure.CatProductElimLeft <$> eraseTerm a <*> eraseTerm s <*> eraseType (Typed.annType ann)
 eraseTerm (Typed.CatProductElimRight a s ann) = do
-  let ty@(IR.VCatProduct _ _) = Typed.annType ann
-  Erasure.CatProductElimRight <$> eraseTerm a <*> eraseTerm s <*> eraseType ty
+  Erasure.CatProductElimRight <$> eraseTerm a <*> eraseTerm s <*> eraseType (Typed.annType ann)
 eraseTerm (Typed.CatCoproductIntroLeft s ann) = do
-  let ty@(IR.VCatCoproduct _ _) = Typed.annType ann
-  Erasure.CatCoproductIntroLeft <$> eraseTerm s <*> eraseType ty
+  Erasure.CatCoproductIntroLeft <$> eraseTerm s <*> eraseType (Typed.annType ann)
 eraseTerm (Typed.CatCoproductIntroRight s ann) = do
-  let ty@(IR.VCatCoproduct _ _) = Typed.annType ann
-  Erasure.CatCoproductIntroRight <$> eraseTerm s <*> eraseType ty
+  Erasure.CatCoproductIntroRight <$> eraseTerm s <*> eraseType (Typed.annType ann)
 eraseTerm (Typed.CatCoproductElim a b cp s t ann) = do
-  let ty@(IR.VCatCoproduct _ _) = Typed.annType ann
-  Erasure.CatCoproductElim <$> eraseTerm a <*> eraseTerm b <*> eraseTerm cp <*> eraseTerm s <*> eraseTerm t <*> eraseType ty
+  Erasure.CatCoproductElim <$> eraseTerm a <*> eraseTerm b <*> eraseTerm cp <*> eraseTerm s <*> eraseTerm t <*> eraseType (Typed.annType ann)
 eraseTerm t@(Typed.UnitTy {}) = throwEra $ Erasure.UnsupportedTermT t
 eraseTerm (Typed.Unit ann) = Erasure.Unit <$> eraseType (Typed.annType ann)
 eraseTerm (Typed.Let π b t anns) = do
