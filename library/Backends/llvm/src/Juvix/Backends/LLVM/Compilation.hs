b660c7ab44bda462f054180aa182f1454888af97
mariari <mariari@protonmail.com>
2021-11-05 12:21:43 -0500

    LLVM: Refactoring the LLVM Compilation

--- library/Backends/llvm/src/Juvix/Backends/LLVM/Compilation.hs
+++ library/Backends/llvm/src/Juvix/Backends/LLVM/Compilation.hs
@@ -1,22 +1,21 @@
-{-# LANGUAGE NamedFieldPuns #-}
-{-# LANGUAGE OverloadedStrings #-}
-
 module Juvix.Backends.LLVM.Compilation
   ( compileProgram,
   )
 where
 
 import qualified Data.String as S (fromString)
+import qualified Juvix.Backends.LLVM.Codegen.Block as Block
+import qualified Juvix.Backends.LLVM.Codegen.Types as Types
 import Juvix.Backends.LLVM.Primitive
 import qualified Juvix.Core.Erased.Ann as ErasedAnn
 import Juvix.Library
-import Juvix.Library.Feedback
+import qualified Juvix.Library.Feedback as Feedback
 import qualified Juvix.Library.HashMap as Map
 import qualified Juvix.Library.NameSymbol as NameSymbol
-import qualified LLVM.AST as LLVM (Module, Name, Operand (..))
+import qualified LLVM.AST as LLVM (Name, Operand (..))
 import qualified LLVM.AST.Constant as LLVM (Constant (..))
 import qualified LLVM.AST.Type as LLVM
-import qualified LLVM.IRBuilder as LLVM
+import qualified LLVM.IRBuilder as LLVMBuild
 import qualified LLVM.Pretty as LLVM
 import qualified Prelude as P
 
@@ -29,10 +28,96 @@ compileProgram ::
   Monad m =>
   -- | Term to compile.
   ErasedAnn.AnnTerm PrimTy RawPrimVal ->
-  FeedbackT [] P.String m Text
+  Feedback.FeedbackT [] P.String m Text
 compileProgram t = do
-  let llvmmod = LLVM.buildModule "juvix-module" $ mkMain t
+  let llvmmod = LLVMBuild.buildModule "juvix-module" $ mkMain t
   return $ toStrict $ LLVM.ppllvm llvmmod
+
+--------------------------------------------------------------------------------
+-- Top Level Compilation
+--------------------------------------------------------------------------------
+
+compileProgram' ::
+  Monad m =>
+  -- | Term to compile.
+  ErasedAnn.AnnTerm PrimTy RawPrimVal ->
+  Feedback.FeedbackT [] P.String m Text
+compileProgram' t =
+  Block.execEnvState (mkMain' t) mempty
+    |> Types.moduleAST
+    |> LLVM.ppllvm
+    |> toStrict
+    |> return
+
+--------------------------------------------------------------------------------
+-- Function Declaration
+--------------------------------------------------------------------------------
+
+mkMain' t@(ErasedAnn.Ann usage ty t') = do
+  let (paramTys, returnTy) = functionType ty
+      returnTy' = typeToLLVM returnTy
+      paramTys' = map typeToLLVM paramTys
+      paramNames =
+        zipWith
+          (\l r -> Block.internName (l <> r))
+          (replicate (length paramTys') "arg")
+          (fmap (intern . show) [1 ..])
+      params = zip paramTys' paramNames
+  Block.defineFunction returnTy' "main" params $
+    do
+      undefined
+
+--------------------------------------------------------------------------------
+-- Term Handling
+--------------------------------------------------------------------------------
+
+compileTerm' (ErasedAnn.Ann _usage ty t) =
+  case t of
+    ErasedAnn.LamM caps as body -> compileLam ty caps as body
+    ErasedAnn.AppM fn arguments -> compileApp ty fn arguments
+    ErasedAnn.Var sym -> compileVar sym
+    ErasedAnn.Prim _t -> mkPrim t ty
+
+compileVar ::
+  (HasState "symTab" Types.SymbolTable m, HasThrow "err" Types.Errors m) =>
+  NameSymbol.T ->
+  m LLVM.Operand
+compileVar sym = do
+  Block.externf (Block.internName (NameSymbol.toSymbol sym))
+
+-- TODO :: compile as closures, with captures
+compileLam ty captures arguments body
+  | length captures == 0 = do
+    let (llvmArgty, llvmRetty) =
+          functionTypeLLVM ty
+        llvmArgNames =
+          fmap (Block.internName . NameSymbol.toSymbol) arguments
+        llvmArguments =
+          zip llvmArgty llvmArgNames
+    -- time to generate unique names
+    lamName <- Block.generateUniqueSymbol "lam"
+    Block.defineFunction llvmRetty lamName llvmArguments $
+      do
+        undefined
+  | otherwise =
+    throw @"err" (Types.UnsupportedOperation "closures are not supported")
+
+compileApp returnTy f@ErasedAnn.Ann {ErasedAnn.term, ErasedAnn.type'} xs =
+  case term of
+    -- we only treat prims specially
+    ErasedAnn.Prim prim ->
+      undefined
+    _ -> do
+      arguments <- traverse compileTerm' xs
+      function <- compileTerm' f
+      --
+      let -- We should probably get the type from the function itself
+          -- rather than pass in what it should be here
+          functionType = typeToLLVM returnTy
+          -- ignore attributes for now!
+          argsAtrributes = zip arguments (repeat [])
+      --
+      Block.call functionType function argsAtrributes
 
 -- | Write the main function of the module. Here two distinct cases can be
 -- observed:
@@ -48,7 +133,7 @@ compileProgram t = do
 -- body of the main function by compiling @t@. The main function itself does
 -- not have any parameters in this case.
 mkMain ::
-  LLVM.MonadModuleBuilder m =>
+  LLVMBuild.MonadModuleBuilder m =>
   -- | Term to compile.
   ErasedAnn.AnnTerm PrimTy RawPrimVal ->
   m LLVM.Operand
@@ -58,7 +143,7 @@ mkMain t@(ErasedAnn.Ann usage ty t') = do
       paramTys' = map typeToLLVM paramTys
       paramNames = repeat "arg"
       params = zip paramTys' (map mkParameterName paramNames)
-  LLVM.function "main" params returnTy' $ \args -> do
+  LLVMBuild.function "main" params returnTy' $ \args -> do
     out <- case t' of
       ErasedAnn.LamM
         { ErasedAnn.capture,
@@ -68,14 +153,14 @@ mkMain t@(ErasedAnn.Ann usage ty t') = do
           let env = Map.fromList $ zip paramNames args
               callArgs = zip args (repeat []) -- No arg attributes.
           funname <- mkLam env ty body arguments capture
-          LLVM.call (globalRef (typeToLLVM ty) funname) callArgs
+          LLVMBuild.call (globalRef (typeToLLVM ty) funname) callArgs
       _ -> compileTerm mempty t
-    LLVM.ret out
+    LLVMBuild.ret out
 
 -- | Compile a term to its equivalent LLVM code.
 -- TODO: implement other constructors of ErasedAnn.Term
 compileTerm ::
-  (LLVM.MonadIRBuilder m, LLVM.MonadModuleBuilder m) =>
+  (LLVMBuild.MonadIRBuilder m, LLVMBuild.MonadModuleBuilder m) =>
   -- | Environment of Juvix variables to LLVM function arguments.
   Env ->
   -- | The term to compile.
@@ -92,7 +177,7 @@ compileTerm env (ErasedAnn.Ann usage ty t) = case t of
 -- | Write an LLVM function definition based on a the given lambda abstraction.
 -- The function returns the name of the create function.
 mkLam ::
-  (LLVM.MonadIRBuilder m, LLVM.MonadModuleBuilder m) =>
+  (LLVMBuild.MonadIRBuilder m, LLVMBuild.MonadModuleBuilder m) =>
   -- | Environment of Juvix variables to LLVM function arguments.
   Env ->
   -- | The type of the lambda abstraction.
@@ -105,15 +190,15 @@ mkLam ::
   [NameSymbol.T] ->
   m LLVM.Name
 mkLam env ty body args capt = do
-  funname <- LLVM.freshName "lam"
+  funname <- LLVMBuild.freshName "lam"
   let (argTys, returnTy) = functionType ty
       returnTy' = typeToLLVM returnTy
       paramNames = repeat "arg"
       params = zipWith mkParameter argTys paramNames
-  LLVM.function funname params returnTy' $ \refs -> do
+  LLVMBuild.function funname params returnTy' $ \refs -> do
     let env' = env `Map.union` Map.fromList (zip args refs)
     body' <- compileTerm env' body
-    LLVM.ret body'
+    LLVMBuild.ret body'
   return funname
 
 -- | Given a juvix name and type, construct an llvm function parameter
@@ -123,13 +208,13 @@ mkParameter ::
   ErasedAnn.Type PrimTy ->
   -- | The name of the parameter.
   NameSymbol.T ->
-  (LLVM.Type, LLVM.ParameterName)
+  (LLVM.Type, LLVMBuild.ParameterName)
 mkParameter ty name = (typeToLLVM ty, mkParameterName name)
 
 -- | The function assumes the arguments passed are the arguments of an
 -- application.
 mkApp ::
-  (LLVM.MonadIRBuilder m, LLVM.MonadModuleBuilder m) =>
+  (LLVMBuild.MonadIRBuilder m, LLVMBuild.MonadModuleBuilder m) =>
   -- | Environment of Juvix variables to LLVM function arguments.
   Env ->
   -- | The function term of an application.
@@ -145,18 +230,18 @@ mkApp env f@(ErasedAnn.Ann {ErasedAnn.term, ErasedAnn.type'}) _ xs =
       funname <- mkLam env type' body arguments capture
       xs' <- mapM (compileTerm env) xs
       let xs'args = zip xs' (repeat [])
-      LLVM.call (globalRef (typeToLLVM type') funname) xs'args
+      LLVMBuild.call (globalRef (typeToLLVM type') funname) xs'args
     ErasedAnn.Prim prim -> applyPrim env prim xs
     ErasedAnn.Var v -> do
       f' <- compileTerm env f
       xs' <- mapM (compileTerm env) xs
       let xs'args = zip xs' (repeat []) -- Do not pass attributes to the args.
-      LLVM.call f' xs'args
+      LLVMBuild.call f' xs'args
 
 -- | Write LLVM code for a primitive.
 -- TODO: implement other primitives.
 mkPrim ::
-  LLVM.MonadIRBuilder m =>
+  Monad m =>
   -- | Term that contains the primitive.
   ErasedAnn.Term PrimTy RawPrimVal ->
   -- | Type of the primitive.
@@ -172,7 +257,7 @@ mkPrim (ErasedAnn.Prim prim) ty = case prim of
 -- | Generate code for primitives that are used as a function.
 -- TODO: implement other primitives.
 applyPrim ::
-  (LLVM.MonadIRBuilder m, LLVM.MonadModuleBuilder m) =>
+  (LLVMBuild.MonadIRBuilder m, LLVMBuild.MonadModuleBuilder m) =>
   -- | Environment of global variables.
   Env ->
   -- | The function primitive of the application.
@@ -186,23 +271,27 @@ applyPrim env f xs
       Add -> do
         x <- compileTerm env (xs P.!! 0)
         y <- compileTerm env (xs P.!! 1)
-        LLVM.add x y
+        LLVMBuild.add x y
       Sub -> do
         x <- compileTerm env (xs P.!! 0)
         y <- compileTerm env (xs P.!! 1)
-        LLVM.sub x y
+        LLVMBuild.sub x y
       Mul -> do
         x <- compileTerm env (xs P.!! 0)
         y <- compileTerm env (xs P.!! 1)
-        LLVM.mul x y
+        LLVMBuild.mul x y
 
 -- | Counterpart of `mkName`: make a `ParameterName` given a name.
-mkParameterName :: NameSymbol.T -> LLVM.ParameterName
+mkParameterName :: NameSymbol.T -> LLVMBuild.ParameterName
 mkParameterName s = S.fromString $ unintern $ NameSymbol.toSymbol s
 
 -- | Handy wrapper for creating global references based on a type and name.
 globalRef :: LLVM.Type -> LLVM.Name -> LLVM.Operand
 globalRef ty name = LLVM.ConstantOperand $ LLVM.GlobalReference ty name
+
+functionTypeLLVM prim =
+  functionType prim
+    |> bimap (fmap typeToLLVM) typeToLLVM
 
 -- | Translate a Juvix type into an LLVM type.
 typeToLLVM :: ErasedAnn.Type PrimTy -> LLVM.Type
