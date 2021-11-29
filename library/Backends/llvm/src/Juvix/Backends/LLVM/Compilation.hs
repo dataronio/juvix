55376679cb534bf4b4607b8eda6b4c599d8c8f44
mariari <mariari@protonmail.com>
2021-11-29 08:10:46 -0600

    LLVM: Closure Implementation code in the compilation module

--- library/Backends/llvm/src/Juvix/Backends/LLVM/Compilation.hs
+++ library/Backends/llvm/src/Juvix/Backends/LLVM/Compilation.hs
@@ -4,8 +4,10 @@ module Juvix.Backends.LLVM.Compilation
 where
 
 import qualified Juvix.Backends.LLVM.Codegen.Block as Block
-import qualified Juvix.Backends.LLVM.Codegen.Types as Types
 import qualified Juvix.Backends.LLVM.Codegen.Closure as Closure
+import qualified Juvix.Backends.LLVM.Codegen.Types as Types
+import qualified Juvix.Backends.LLVM.Pass.ClosureConversion as ClosureConversion
+import qualified Juvix.Backends.LLVM.Pass.Types as Types
 import Juvix.Backends.LLVM.Primitive
 import qualified Juvix.Core.Erased.Ann as ErasedAnn
 import Juvix.Library
@@ -15,11 +17,9 @@ import qualified LLVM.AST as LLVM (Definition, Operand (..))
 import qualified LLVM.AST.Constant as LLVM (Constant (..))
 import qualified LLVM.AST.Name as Name
 import qualified LLVM.AST.Type as LLVM
-import LLVM.IRBuilder.Module
+import qualified LLVM.AST.Typed as Typed
 import qualified LLVM.Pretty as LLVM
 import qualified Prelude as P
-import qualified Juvix.Backends.LLVM.Pass.Types as Types
-import qualified Juvix.Backends.LLVM.Pass.ClosureConversion as ClosureConversion
 
 --------------------------------------------------------------------------------
 -- Top Level Compilation
@@ -110,7 +110,26 @@ compileTerm (Types.Ann _usage ty t) =
     Types.LamM as body -> compileLam ty as body
     Types.AppM fn arguments -> compileApp ty fn arguments
     Types.Var sym -> compileVar sym
+    Types.ArrayIndex index ->
+      compileIndex ty index
+    Types.Closure cap arg body ->
+      compileClosure ty cap arg body
     Types.Prim _t -> mkPrim t ty
+
+-- | @compileTermForApplication@ like @compileTerm@ however it will
+-- promote lambdas to closures to make the calling convention for HOF proper.
+compileTermForApplication ::
+  Types.Define m => Types.Annotated Types.TermClosure -> m LLVM.Operand
+compileTermForApplication ann@(Types.Ann _usage ty t) =
+  case t of
+    Types.LamM as body -> do
+      lam <- compileLam ty as body
+      -- WARNING ∷ Leaking memory
+      ------------------------------
+      location <- Closure.malloc
+      Closure.storeFunctionPtr location lam
+      pure location
+    _ -> compileTerm ann
 
 compileVar ::
   (HasState "symTab" Types.SymbolTable m, HasThrow "err" Types.Errors m) =>
@@ -133,18 +152,32 @@ compileLam ::
   Types.Annotated Types.TermClosure ->
   m LLVM.Operand
 compileLam ty arguments body = do
-    let (llvmArgty, llvmRetty) =
-          functionTypeLLVM ty
-        llvmArgNames =
-          fmap (Block.internName . NameSymbol.toSymbol) arguments
-        llvmArguments =
-          zip llvmArgty llvmArgNames
-    -- time to generate unique names
-    lamName <- Block.generateUniqueSymbol "lambda"
-    Block.defineFunction llvmRetty lamName llvmArguments $
-      do
-        bod <- compileTerm body
-        Block.ret bod
+  lamName <- Block.generateUniqueSymbol "lambda"
+  compileFunctionEnv lamName ty arguments $
+    do
+      bod <- compileTerm body
+      Block.ret bod
+
+compileClosure ::
+  Types.Define m =>
+  ErasedAnn.Type PrimTy ->
+  [Types.Capture] ->
+  [NameSymbol.T] ->
+  Types.Annotated Types.TermClosure ->
+  m LLVM.Operand
+compileClosure ty captures args body = do
+  name <- Block.generateUniqueSymbol "closure"
+  fnPtr <- compileFunctionEnv name ty args (compileTerm body >>= Block.ret)
+  -- WARNING ∷ Linking Memory
+  ------------------------------
+  closure <- Closure.malloc
+  Closure.storeFunctionPtr closure fnPtr
+  -- WARNING ∷ Linking Memory
+  ------------------------------
+  capt <- captureToConversion captures
+  Closure.storeEnvironmentPtr closure capt
+  --
+  return closure
 
 -- | The function assumes the arguments passed are the arguments of an
 -- application.
@@ -163,17 +196,44 @@ compileApp returnTy f@Types.Ann {term} xs =
     Types.Prim prim ->
       compilePrimApp returnTy prim xs
     _ -> do
-      arguments <- traverse compileTerm xs
+      arguments <- traverse compileTermForApplication xs
       -- do a case on the function itself to see if it's a closure!
       function <- compileTerm f
       --
       let -- We should probably get the type from the function itself
           -- rather than pass in what it should be here
-          functionType = typeToLLVM returnTy
+          newReturnType = typeToLLVM returnTy
           -- ignore attributes for now!
           argsAtrributes = zip arguments (repeat [])
       --
-      Block.call functionType function argsAtrributes
+      case Typed.typeOf function of
+        -- Closure calling case
+        LLVM.PointerType (LLVM.NamedTypeReference {}) _ -> do
+          let argumentTypes =
+                Closure.environmentPtr :
+                llvmFunctionsToClosure (fmap (typeToLLVM . Types.annTy) xs)
+              functionType =
+                LLVM.FunctionType newReturnType argumentTypes False
+          --------------------------------
+          -- Function Pointer
+          --------------------------------
+          functionPointer <-
+            Closure.loadFunctionPtr function (Types.pointerOf functionType)
+          --------------------------------
+          -- Environment Pointer
+          ------------------------------
+          environmentPointer <- Closure.loadEnvironmentPtr function
+          --
+          let argsWithEnv = (environmentPointer, []) : argsAtrributes
+          --
+          Block.call newReturnType functionPointer argsWithEnv
+        -- we have a valid function pointer, let us now call it with a
+        -- null environment
+        _ -> do
+          let argsWithEnv =
+                (Block.null Closure.environmentPtr, []) : argsAtrributes
+          c <- Block.call newReturnType function argsWithEnv
+          return c
 
 compilePrimApp ::
   Types.Define m =>
@@ -205,6 +265,14 @@ compilePrimApp ty f xs
           ("Was expecting " <> show (arityRaw f) <> "but got " <> show (lengthN xs))
       )
 
+compileIndex ::
+  Types.Call m => ErasedAnn.Type PrimTy -> Types.IndexInto -> m LLVM.Operand
+compileIndex ty index = do
+  let newTy = typeToLLVM ty
+  closurePtr <- loadElementIndex index
+  cast <- Block.bitCast closurePtr (Types.pointerOf newTy)
+  Block.load newTy cast
+
 -- | Write LLVM code for a primitive.
 -- TODO: implement other primitives.
 mkPrim ::
@@ -225,6 +293,117 @@ mkPrim (Types.Prim prim) ty = case prim of
     ErasedAnn.PrimTy (PrimTy (LLVM.PointerType (LLVM.IntegerType 8) _)) -> do
       name <- Block.generateUniqueName "LitString"
       Block.globalString (toS s) name
+
+--------------------------------------------------------------------------------
+-- Capture Conversion
+--------------------------------------------------------------------------------
+
+captureToConversion :: Types.Call m => [Types.Capture] -> m LLVM.Operand
+captureToConversion xs = do
+  ------------------------------
+  -- WARNING ∷ Leaking Memory
+  ------------------------------
+  env <- Closure.mallocEnvironment (fromIntegral (length xs))
+  let insertInto (Types.Capture {slot = Types.Slot {newIndex}, location}) = do
+        capturedArg <-
+          case location of
+            Types.FromAmbientEnv name -> do
+              var <- compileVar name
+              ------------------------------
+              -- WARNING ∷ Leaking Memory
+              ------------------------------
+              loc <- Block.mallocType (Typed.typeOf var)
+              Block.store loc var
+              pure loc
+            Types.FromClosureEnv index ->
+              loadElementIndex index
+        locPtr <-
+          Block.getElementPtr
+            Types.Minimal
+              { type' = Closure.environmentPtr,
+                address' = env,
+                indincies' =
+                  Block.constant32List [fromIntegral (Types.num newIndex)]
+              }
+        Block.store locPtr capturedArg
+  traverse_ insertInto xs
+  pure env
+
+--------------------------------------------------------------------------------
+-- Closure Argument Names
+--------------------------------------------------------------------------------
+
+closureArgumentName :: IsString p => p
+closureArgumentName = "juvix_argArray"
+
+closureCaptureName :: IsString p => p
+closureCaptureName = "juvix_environmentArray"
+
+--------------------------------------------------------------------------------
+-- Closure Indexing
+--------------------------------------------------------------------------------
+
+loadElementIndex :: Types.Call m => Types.IndexInto -> m LLVM.Operand
+loadElementIndex Types.IndexInto {index, into} = do
+  arr <-
+    case into of
+      Types.ClosureEnvironment ->
+        compileVar closureCaptureName
+      -- Does not happen in the current model, check in later for when
+      -- we can properly do this!
+      Types.ArgumentEnvironemnt ->
+        compileVar closureArgumentName
+  Block.loadElementPtr
+    Types.Minimal
+      { type' = Closure.environmentPtrDeref,
+        address' = arr,
+        indincies' = Block.constant32List [fromIntegral (Types.num index)]
+      }
+
+--------------------------------------------------------------------------------
+-- Function Definition Helpers
+--------------------------------------------------------------------------------
+
+compileFunctionEnv ::
+  Types.Define m =>
+  -- | Function name
+  Symbol ->
+  -- | The type of the lambda abstraction.
+  ErasedAnn.Type PrimTy ->
+  -- | List of parameter names.
+  [NameSymbol.T] ->
+  -- | The body of the lambda abstraction.
+  m a ->
+  m LLVM.Operand
+compileFunctionEnv name ty arguments body = do
+  let (llvmArgtyBeforeClosure, llvmRetty) =
+        functionTypeLLVM ty
+      llvmArgNames =
+        fmap (Block.internName . NameSymbol.toSymbol) arguments
+      -- turn the function pointers to closure for any HOF
+      llvmArgty =
+        llvmFunctionsToClosure llvmArgtyBeforeClosure
+      llvmArguments =
+        zip llvmArgty llvmArgNames
+      argumentsWithEnv =
+        (Closure.environmentPtr, closureCaptureName) : llvmArguments
+  -- time to generate unique names
+  Block.defineFunction llvmRetty name argumentsWithEnv $
+    body
+
+--------------------------------------------------------------------------------
+-- Type Helper Functions
+--------------------------------------------------------------------------------
+
+llvmFunctionsToClosure :: Functor f => f LLVM.Type -> f LLVM.Type
+llvmFunctionsToClosure xs =
+  fmap f xs
+  where
+    f x =
+      case x of
+        LLVM.PointerType (LLVM.FunctionType {}) _ ->
+          Closure.pointer
+        _ -> x
 
 functionTypeLLVM :: ErasedAnn.Type PrimTy -> ([LLVM.Type], LLVM.Type)
 functionTypeLLVM prim =
