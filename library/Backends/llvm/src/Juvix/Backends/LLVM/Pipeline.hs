338d9a548c20aecac7f931420ad7a96e5d79b6ef
mariari <mariari@protonmail.com>
2021-12-08 14:42:42 -0600

    LLVM: qualify pipeline moudle

--- library/Backends/llvm/src/Juvix/Backends/LLVM/Pipeline.hs
+++ library/Backends/llvm/src/Juvix/Backends/LLVM/Pipeline.hs
@@ -8,9 +8,9 @@ module Juvix.Backends.LLVM.Pipeline
 where
 
 import qualified Data.Aeson as A
-import Juvix.Backends.LLVM.Compilation
-import Juvix.Backends.LLVM.Parameterization
-import Juvix.Backends.LLVM.Primitive
+import qualified Juvix.Backends.LLVM.Compilation as Compiliation
+import qualified Juvix.Backends.LLVM.Parameterization as Parameterization
+import qualified Juvix.Backends.LLVM.Primitive as Primitive
 import qualified Juvix.Core.Erased.Ann as ErasedAnn
 import Juvix.Library
 import qualified Juvix.Pipeline as Pipeline
@@ -20,16 +20,16 @@ data BLLVM = BLLVM
   deriving (Show, Eq, Generic, A.ToJSON, A.FromJSON)
 
 instance Pipeline.HasBackend BLLVM where
-  type Ty BLLVM = PrimTy
-  type Val BLLVM = RawPrimVal
-  type Err BLLVM = CompilationError
+  type Ty BLLVM = Primitive.PrimTy
+  type Val BLLVM = Primitive.RawPrimVal
+  type Err BLLVM = Primitive.CompilationError
 
   stdlibs _ = ["LLVM.ju", "LLVM/Int.ju"]
 
-  param _ = llvm
+  param _ = Parameterization.llvm
 
-  typecheck ctx = Pipeline.typecheck' ctx llvm
+  typecheck ctx = Pipeline.typecheck' ctx Parameterization.llvm
 
   compile' term = do
     let raw = ErasedAnn.toRaw term
-    compileProgram raw
+    Compiliation.compileProgram raw
